96d55df465b243a06c74e6864dcb6babf72aee3b
Hubert Barc <barchubert@gmail.com>
2023-12-07 18:05:45 +0100

    day7

--- day07/target/debug/day07.d
+++
@@ -1,1 +0,0 @@
-/home/des/project/aoc2023/day07/target/debug/day07: /home/des/project/aoc2023/day07/src/main.rs
--- day07/target/debug/deps/day07-dcfa880369002869.d
+++
@@ -1,5 +0,0 @@
-/home/des/project/aoc2023/day07/target/debug/deps/day07-dcfa880369002869.rmeta: src/main.rs
-
-/home/des/project/aoc2023/day07/target/debug/deps/day07-dcfa880369002869.d: src/main.rs
-
-src/main.rs:
